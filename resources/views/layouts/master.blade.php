799145ca735812f9ed0205912cf5a9858e6569f3
Rovan Wardana <rovanwardana@gmail.com>
2025-08-25 15:23:24 +0800

    favicon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Landing Page')</title>
+    <link rel="icon" type="png" href="{{ asset('images/Logo-BMKG.png') }}">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
